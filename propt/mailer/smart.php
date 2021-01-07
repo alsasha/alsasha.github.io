7337cbf546b62a61e4bb467b24330cf8fc4e25ff
Alfiia <alfiyaenikeeva1@gmail.com>
2021-01-07 22:45:23 +0500

    ddgdbg

--- propt/mailer/smart.php
+++ propt/mailer/smart.php
@@ -1,15 +1,10 @@
-<?php
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\Exception;
-
-require 'PHPMailer/src/Exception.php';
-require 'PHPMailer/src/PHPMailer.php';
-require 'PHPMailer/src/SMTP.php';
+<?php 
 
 $name = $_POST['name'];
 $phone = $_POST['phone'];
 $email = $_POST['email'];
 
+require_once('phpmailer/PHPMailerAutoload.php');
 $mail = new PHPMailer;
 $mail->CharSet = 'utf-8';
 
